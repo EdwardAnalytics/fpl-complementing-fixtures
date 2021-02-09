6e6a10265ffc691c1e551ac533b80f3e4549a08d
efarra <43321188+efarra@users.noreply.github.com>
2021-02-09 11:00:15 +0000

    Updated following development

---
+++ refresh_data.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from datetime import datetime
+
+# Get current date to timestamp data
+date = datetime.today().strftime("%Y%m%d")
+
+### Bootstrap-static
+# Load JSON
+url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+data = requests.get(url)
+data = data.json()
+
+# Write JSON
+path = f'data/bootstrap_static/bootstrap_static_{date}.json'
+with open(path, 'w') as f:
+    json.dump(data, f)
+
+### Fixtures
+# Load JSON
+url = 'https://fantasy.premierleague.com/api/fixtures/'
+data = requests.get(url)
+data = data.json()
+
+# Write JSON
+path = f'data/fixtures/fixtures_{date}.json'
+with open(path, 'w') as f:
+    json.dump(data, f)
